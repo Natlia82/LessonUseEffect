47e0135ba965462b0b0111c82dd7f59fc6d817fb
Natalia <nata@host>
2023-04-03 21:03:40 +0500

    без ошибок идет загрузка

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import Details from './Details';
 
 function List() {
@@ -8,42 +8,54 @@ function List() {
     const [url, setUrl] = useState("https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json");
     const [img, setImg] = useState({});
     const [id, setId] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const timestampRef =useRef();
 
     useEffect(() => {
         
         const cont = async() => {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
-            const rez = await response.json();
-            if (url == "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json") {
-                setFio(rez);
+            const timestap = Date.now();
+            timestampRef.current = timestap;
+            setLoading(false);
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                const rez = await response.json();
+                if (timestampRef.current === timestap) {
+                    if (url == "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json") {
+                       setFio(rez);
                 
-            } else {
-             //  console.log(url);
-                setImg(rez);
-              //  console.log(rez);
-               // console.log(img);
-            }
+                    } else {
+                        setImg(rez);
+                        setLoading(true);
+                    }
+                    
+                }
+              //  setError(e);
             
+        } catch(e) {
+            console.log(e);
+        } finally {
+            
+        }
            
         }
         cont();
         return setImg({});
     } , [url, id]);
    
-    console.log(img);
-    /*if (id > 0) {
-       console.log(img.details.city);
-    }*/
+   
 
     const fioClick = evt => {
         const kod = evt.target.getAttribute('id');
-        console.log(kod);
-       // console.log(img.details.city);
-        setUrl("https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/"+kod+".json");
-        setId(kod);
+      
+        if (id != kod) {
+            setUrl("https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/"+kod+".json");
+            setId(kod);
+        }
+        
     };
 
 
@@ -54,6 +66,7 @@ function List() {
                 {fio.map(o => <ListGroup.Item onClick={(evt) => fioClick(evt)} key={o.id} id={o.id}>{o.name}</ListGroup.Item>)}
             </ListGroup>
         </div>
+        {loading? 
         <div className='list'>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={img.avatar} />
@@ -61,12 +74,14 @@ function List() {
                     <Card.Title>{img.name}</Card.Title>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                    {/*img.details? <ListGroup.Item>city: {img.details.city}</ListGroup.Item>:""*/}
+                    <ListGroup.Item>city: {img.details.city}</ListGroup.Item>
+                    <ListGroup.Item>company: {img.details.company}</ListGroup.Item>
+                    <ListGroup.Item>position: {img.details.position}</ListGroup.Item>
                 </ListGroup>
       
             </Card>
             
-        </div>
+        </div>: <div>Идет загрузка..</div> }
       </div>
      
         
